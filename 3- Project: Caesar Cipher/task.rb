5d28459f358b7257c25d941e576aea4a065e5dc0
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-02-13 16:39:21 -0600

    Ejercicio terminado

---
+++ 3- Project: Caesar Cipher/task.rb
@@ -0,0 +1,17 @@
+def caesar_cipher(phrase, number)
+  alphabet = ("a".."z").to_a
+  array_phrase = phrase.downcase.split("")
+  result = []
+  array_phrase.each do |letter|
+    if alphabet.include?(letter)
+      new_index = alphabet.index(letter) - number
+      result << alphabet[new_index]
+    else
+      result << letter
+    end
+  end
+
+p result.join("")
+end
+
+caesar_cipher("hola como estas?",1)
